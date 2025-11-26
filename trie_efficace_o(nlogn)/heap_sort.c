bdde60e9a763529edb46df216ccf252da2b46bf3
Claude_Delcroix <166008956+claudeDelcroix@users.noreply.github.com>
2025-11-26 18:20:23 +0300

    Create heap_sort.c

---
+++ trie_efficace_o(nlogn)/heap_sort.c
@@ -0,0 +1,31 @@
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int tmp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = tmp;
+        heapify(arr, n, largest);
+    }
+}
+
+void heapsort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        int tmp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = tmp;
+
+        heapify(arr, i, 0);
+    }
+}
